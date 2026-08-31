52f26ac9dbb2ac47f885236e37bae43523bc3edb
bhavyasahni123 <bhavyasahni012@gmail.com>
2026-08-31 06:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.38%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    int low=1;
+    int high=x;
+    int ans=0;
+
+    if(x>3){
+        int high=x/2;
+        while(low<=high){
+        long long mid=low+(high-low)/2;
+        if(mid*mid<=x){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            {
+                high=mid-1;
+            }
+        }
+
+    }
+    return ans;
+    } 
+    
+    else{while(low<=high){
+        long long mid=low+(high-low)/2;
+        if(mid*mid<=x){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            {
+                high=mid-1;
+            }
+        }
+
+    }
+    return ans;
+    } }
+
+    
+};
